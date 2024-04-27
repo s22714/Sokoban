cd45c8c6f561317a3e47fddc06b7e53d64db1210
s22714 <s22714@pjwstk.edu.pl>
2024-04-27 11:14:42 +0200

    init

---
+++ Assets/Scripts/LevelLoaderScript.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LevelLoaderScript : MonoBehaviour
+{
+    private string _levelsPath = "Assets/Levels/levels.txt";
+    [SerializeField] private int _levelToLoad = 0;
+    private bool _recordLevel = false;
+
+    [SerializeField] private GameObject _grassBlock;
+    [SerializeField] private GameObject _wallBlock;
+    [SerializeField] private GameObject _crateBlock;
+    [SerializeField] private GameObject _storageBlock;
+    [SerializeField] private GameObject _playerPrefab;
+    [SerializeField] private GameObject _parent;
+
+    [SerializeField] private GameObject _gridShader;
+
+    [SerializeField] private Camera _camera;
+
+    private void Awake()
+    {
+        var arr = GetLevelArray(_levelToLoad);
+        InstantiateLevel(arr);
+    }
+
+    private List<List<char>> GetLevelArray(int _levelToLoad)
+    {
+        var level = new List<List<char>>();
+
+        try
+        {
+            StreamReader sr = new(_levelsPath);
+
+            var line = sr.ReadLine();
+            while(line != null)
+            {
+                
+                level.Add(new());
+
+                foreach( char c in line)
+                {
+                    level[level.Count - 1].Add(c);
+                }
+
+                line = sr.ReadLine();
+            }
+            sr.Close();
+        }
+        catch(Exception ex)
+        {
+            Debug.Log("ex: " + ex.Message);
+        }
+
+        return level;
+    }
+
+    private void InstantiateLevel(List<List<char>> arr)
+    {
+
+        for(int i = 0; i < arr.Count; i++)
+        {
+            for(int j = 0; j < arr[i].Count; j++)
+            {
+                Instantiate(_grassBlock, new Vector2(j, arr[i].Count - i),new Quaternion(),_parent.transform);
+                switch (arr[i][j])
+                {
+                    case 'x':
+                        Instantiate(_playerPrefab, new Vector2(j, arr[i].Count - i), new Quaternion(), _parent.transform);
+                        break;
+                    case '#':
+                        Instantiate(_wallBlock, new Vector2(j, arr[i].Count - i), new Quaternion(), _parent.transform);
+                        break;
+                    case '*':
+                        Instantiate(_crateBlock, new Vector2(j, arr[i].Count - i), new Quaternion(), _parent.transform);
+                        break;
+                    case 'o':
+                        Instantiate(_storageBlock, new Vector2(j, arr[i].Count - i), new Quaternion(), _parent.transform);
+                        break;
+                    default: 
+                        break;
+                }
+            }
+        }
+
+        var shPos = _gridShader.transform;
+        if(arr.Count % 2 == 0)
+            shPos.position = new Vector2(arr[0].Count/2 - 0.5f, arr.Count / 2 + 0.5f);
+        else
+        {
+            shPos.position = new Vector2(arr[0].Count / 2, arr.Count / 2 + 1);
+        }
+        shPos.localScale = new Vector3(arr[0].Count, arr.Count, arr.Count);
+
+        _camera.transform.position = new Vector3(shPos.position.x,shPos.position.y,-1);
+        _camera.orthographicSize = arr.Count/2 + 1;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D _rigidBody2d;
+
+    void Start()
+    {
+        _rigidBody2d = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
